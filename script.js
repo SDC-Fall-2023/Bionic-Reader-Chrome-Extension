5a79d09ab19c542ee50d8a4a65777a0e1496e71a
jackabald <jwarchibald@wisc.edu>
2023-11-14 20:15:18 -0600

    Bionic reader On/Off button

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-async function fetchData() {
-    const res = await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record = await res.json();
-    console.log(record);
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();
+document.addEventListener("DOMContentLoaded", function () {
+  var bionicReaderEnabled = false;
+
+  // Function to toggle the Bionic Reader state
+  function toggleBionicReader() {
+    bionicReaderEnabled = !bionicReaderEnabled;
+    var toggleBtn = document.getElementById("toggleBtn");
+
+    if (bionicReaderEnabled) {
+      toggleBtn.textContent = "Bionic Reader: ON";
+      // Add logic to enable Bionic Reader functionality here
+    } else {
+      toggleBtn.textContent = "Bionic Reader: OFF";
+      // Add logic to disable Bionic Reader functionality here
+    }
+  }
+
+  // Add click event listener to the button
+  document
+    .getElementById("toggleBtn")
+    .addEventListener("click", toggleBionicReader);
+});
