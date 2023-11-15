32516108a8d9520b9e5b9f582cfcc4310b797b30
jackabald <jwarchibald@wisc.edu>
2023-11-14 20:33:57 -0600

    Added UI for changing themes

--- script.js
+++ script.js
@@ -7,16 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
     var toggleBtn = document.getElementById("toggleBtn");
 
     if (bionicReaderEnabled) {
-      toggleBtn.textContent = "Bionic Reader: ON";
+      toggleBtn.textContent = "Bionic Text: ON";
       // Add logic to enable Bionic Reader functionality here
     } else {
-      toggleBtn.textContent = "Bionic Reader: OFF";
+      toggleBtn.textContent = "Bionic Text: OFF";
       // Add logic to disable Bionic Reader functionality here
     }
   }
-
   // Add click event listener to the button
   document
     .getElementById("toggleBtn")
     .addEventListener("click", toggleBionicReader);
+
+  // Add input event listeners to color pickers
+  document
+    .getElementById("backgroundColorPicker")
+    .addEventListener("input", handleBackgroundColorChange);
+  document
+    .getElementById("textColorPicker")
+    .addEventListener("input", handleTextColorChange);
 });
